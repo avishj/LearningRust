26c385519bdee270e307c429193a9f1e42bd7727
Avish Jha <avish.j@protonmail.com>
2023-05-07 20:19:03 +0530

    complete 12doc

--- twelve_days_of_christmas_lyrics/src/main.rs
+++ twelve_days_of_christmas_lyrics/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    const DAYS: [&str; 12] = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "ninht",
+        "tenth", "eleventh", "twelveth",
+    ];
+    const PASSAGES: [&str; 12] = [
+        "A partridge in a pear tree",
+        "Two turtle doves, and",
+        "Three french hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    for (i, day) in DAYS.iter().enumerate() {
+        println!("On the {day} day of Christmas, my true love sent to me");
+        for j in (0..=i).rev() {
+            println!("{}", PASSAGES[j])
+        }
+        println!();
+    }
 }
